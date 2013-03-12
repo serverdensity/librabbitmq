75e4bcd57573d0a5e7890379ff50a5ddc4143629
Wes Mason <wes@1stvamp.org>
2013-03-12 10:24:33 +0000

    modify paths some more

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ from setuptools.command.install import install as _install
 
 # --with-librabbitmq=<dir>: path to librabbitmq package if needed
 
-LRMQDIST = lambda *x: os.path.join('./', *x)
-LRMQSRC = lambda *x: os.path.join('./rabbitmq-c/librabbitmq', *x)
+LRMQDIST = lambda *x: os.path.join('', *x)
+LRMQSRC = lambda *x: os.path.join('rabbitmq-c/librabbitmq', *x)
 SPECPATH = lambda *x: os.path.join('rabbitmq-codegen', *x)
 PYCP = lambda *x: os.path.join('Modules', '_librabbitmq', *x)
 
